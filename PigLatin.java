6066068979ac22689db0dcc60941baec45f6f1d2
iwei20 <44007607+ivanwei20@users.noreply.github.com>
2021-01-04 02:18:46 -0500

    Implement part 2

--- PigLatin.java
+++ PigLatin.java
@@ -4,10 +4,11 @@ public class PigLatin {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         while(s.hasNextLine()) {
-            System.out.println(pigLatinSimple(s.nextLine()));
+            System.out.println(pigLatin(s.nextLine()));
         }
         s.close();
     }
+
     public static String pigLatinSimple(String s) {
         String lower = s.toLowerCase();
         char[] vowels = new char[]{'a','e','i','o','u'};
@@ -17,4 +18,16 @@ public class PigLatin {
         return lower.substring(1) + lower.charAt(0) + "ay";
     }
     
+    public static String pigLatin(String s) {
+        String lower = s.toLowerCase();
+        String[] digraphs = new String[]{"bl", "br", "ch", "ck", "cl", "cr", "dr", "fl", "fr", "gh", "gl", "gr", "ng", "ph", "pl", "pr", "qu", "sc", "sh", "sk", "sl", "sm", "sn", "sp", "st", "sw", "th", "tr", "tw", "wh", "wr"};
+        char[] vowels = new char[]{'a','e','i','o','u'};
+        if(Arrays.binarySearch(digraphs, lower.substring(0, 2)) >= 0) {
+            return lower.substring(2) + lower.substring(0, 2) + "ay";
+        }
+        if(Arrays.binarySearch(vowels, lower.charAt(0)) >= 0) {
+            return lower + "hay";
+        }
+        return lower.substring(1) + lower.charAt(0) + "ay";
+    }
 }
